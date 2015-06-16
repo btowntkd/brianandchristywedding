7138f2d64b1accef2063f4e648b09fb809ecbb97
Brian Pratt <bcpratt@live.com>
2015-06-15 22:32:46 -0400

    Added Invitations controller and scaffolds.

--- BrianChristyWedding/DAL/WeddingContext.cs
+++ BrianChristyWedding/DAL/WeddingContext.cs
@@ -19,10 +19,15 @@ namespace BrianChristyWedding.DAL
 
             //One to zero-or-one relationship between invitation and RSVP
             modelBuilder.Entity<Rsvp>().HasKey(x => x.InvitationID);
-
             modelBuilder.Entity<Rsvp>()
                 .HasRequired(x => x.Invitation)
                 .WithOptional(y => y.Rsvp);
+
+            //One to many reltionship between RSVP and Guests
+            modelBuilder.Entity<Guest>()
+                .HasRequired(x => x.Rsvp)
+                .WithMany(y => y.Guests)
+                .HasForeignKey(z => z.RsvpID);
         }
     }
 }
