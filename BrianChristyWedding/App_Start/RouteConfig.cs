1282a86fab3afadc91dcab13e15393692990a721
Brian Pratt <bpratt@pyramidsolutions.com>
2015-06-22 13:48:44 -0400

    RVP routing is working!

--- BrianChristyWedding/App_Start/RouteConfig.cs
+++ BrianChristyWedding/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace BrianChristyWedding
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "InvitationCodePrompt",
+                url: "rsvp/",
+                defaults: new { controller = "Rsvp", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "RSVP",
+                url: "rsvp/{shortcode}",
+                defaults: new { controller = "Rsvp", action = "Rsvp" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ BrianChristyWedding/Controllers/RsvpController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrianChristyWedding.DAL;
+
+namespace BrianChristyWedding.Controllers
+{
+    public class RsvpController : Controller
+    {
+        private WeddingContext db = new WeddingContext();
+
+        // GET: Rsvp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Rsvp
+        public ActionResult Rsvp(string shortcode)
+        {
+            var invitation = db.Invitations.FirstOrDefault(x => x.Shortcode == shortcode);
+            return View(invitation);
+        }
+    }
+}
