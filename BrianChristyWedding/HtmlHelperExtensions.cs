e4ee96909a0db598b40664b38fb4311a46de3158
Brian Pratt <bcpratt@live.com>
2016-01-14 21:09:03 -0500

    Fixed some layout issues.

---
+++ BrianChristyWedding/HtmlHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrianChristyWedding
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ActiveController(this HtmlHelper helper, string controller, string classStyle)
+        {
+            string currentController = helper.ViewContext.Controller.ValueProvider.GetValue("controller").RawValue.ToString();
+            string currentAction = helper.ViewContext.Controller.ValueProvider.GetValue("action").RawValue.ToString();
+
+            if (currentController == controller)
+            {
+                return classStyle;
+            }
+
+            return "";
+        }
+    }
+}
