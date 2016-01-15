1abc90123d597be50c8fbf30875cce174ff883a4
Brian Pratt <bcpratt@live.com>
2016-01-14 22:15:01 -0500

    Added RSVP routing and lookup and view.

--- BrianChristyWedding/App_Start/RouteConfig.cs
+++ BrianChristyWedding/App_Start/RouteConfig.cs
@@ -14,10 +14,14 @@ namespace BrianChristyWedding
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "RsvpShortcode",
+                url: "rsvp/{shortcode}",
+                defaults: new { controller = "Home", action = "Rsvp", shortcode = UrlParameter.Optional });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
--- BrianChristyWedding/Controllers/HomeController.cs
+++ BrianChristyWedding/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BrianChristyWedding.Models;
 
 namespace BrianChristyWedding.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly WeddingContext db = new WeddingContext();
+
         public ActionResult Index()
         {
             return View();
@@ -20,6 +23,18 @@ namespace BrianChristyWedding.Controllers
 
         public ActionResult Contact()
         {
+            return View();
+        }
+
+        public ActionResult Rsvp(string shortcode)
+        {
+            ViewBag.Shortcode = shortcode;
+
+            if (!string.IsNullOrWhiteSpace(shortcode))
+            {
+                var invitation = db.Invitations.FirstOrDefault(x => x.Shortcode == shortcode);
+                return View(invitation);
+            }
             return View();
         }
     }
