51396e190bbfa4789bd9611f4780168d3d22646b
Brian Pratt <bpratt@pyramidsolutions.com>
2016-01-20 13:49:40 -0500

    Implemented the "Contact" page, controller actions and views.

--- BrianChristyWedding/Controllers/HomeController.cs
+++ BrianChristyWedding/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BrianChristyWedding.Models;
+using System.Net.Mail;
 
 namespace BrianChristyWedding.Controllers
 {
@@ -21,8 +22,41 @@ namespace BrianChristyWedding.Controllers
             return View();
         }
 
+        
         public ActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact([Bind(Include = "Name,Email,Subject,Body")] ContactViewModel contactForm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var smtp = new SmtpClient())
+                    {
+                        var mail = new MailMessage();
+                        mail.To.Add("bcpratt@live.com");
+                        mail.To.Add("chrispy115@gmail.com");
+                        mail.Subject = "Message from Wedding Website";
+                        mail.Body = string.Format("<p>Message from {0} ({1})</p><p>Subject: {2}</p><p></p><p>{3}</p>",
+                            contactForm.Name,
+                            contactForm.Email,
+                            contactForm.Subject,
+                            contactForm.Body);
+                        mail.IsBodyHtml = true;
+
+                        smtp.Send(mail);
+                    }
+                    return View("ContactSuccess");
+                }
+                catch (Exception)
+                {
+                    return View("ContactError");
+                }
+            }
             return View();
         }
 
---
+++ BrianChristyWedding/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BrianChristyWedding.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "Your name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Your email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Body { get; set; }
+    }
+}
